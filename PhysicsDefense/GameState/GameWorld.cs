ddf1ed1cc346fc915b7a73266d3b412304f9212b
izenja <izenja@3d5b969a-315a-e0a3-cde1-6223939dcf86>
2011-02-27 01:01:26 +0000

    - Added more basic game architecture - Marble object is now created, drawn, and moving

--- PhysicsDefense/GameState/GameWorld.cs
+++ PhysicsDefense/GameState/GameWorld.cs
@@ -4,21 +4,44 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
 using PhysicsDefense.Physics;
+using PhysicsDefense.Graphics;
 
 namespace PhysicsDefense.GameState
 {
 	class GameWorld
 	{
 		PhysicsSystem physics;
+		GraphicsEngine graphics;
 
-		public GameWorld()
+		List<GameObject> entities;
+
+		public GameWorld(GraphicsEngine graphics)
 		{
+			this.graphics = graphics;
 			physics = new PhysicsSystem();
+			entities = new List<GameObject>();
+
+			// Add a temporary test enemy
+			Marble test = EnemyFactory.createMarble(new Vector2(100f, 50f), physics);
+			addObject(test);
 		}
 
 		public void Update(GameTime gameTime)
 		{
+			// Update physics
 			physics.Update(gameTime);
+
+			// Update game objects
+			foreach (GameObject obj in entities) {
+				obj.update();
+			}
+		}
+
+		private void addObject(GameObject obj)
+		{
+			entities.Add(obj);
+			graphics.addObject(obj);
+			physics.addPhysical(obj);
 		}
 	}
 }
---
+++ PhysicsDefense/Physics/ObjectPhysicsProperties.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FarseerPhysics.Dynamics;
+
+namespace PhysicsDefense.Physics
+{
+	class ObjectPhysicsProperties
+	{
+		public Fixture fixture;
+	}
+}
