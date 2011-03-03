d4758f2d798a8b569a961d6ac12ddb915b33038c
izenja <izenja@3d5b969a-315a-e0a3-cde1-6223939dcf86>
2011-03-03 18:49:11 +0000

    Added box

--- PhysicsDefense/GameState/EnemyFactory.cs
+++ PhysicsDefense/GameState/EnemyFactory.cs
@@ -9,14 +9,26 @@ using Microsoft.Xna.Framework;
 
 namespace PhysicsDefense.GameState
 {
+	/// <summary>
+	/// Creates enemies.
+	/// Will need to be redone so it's more like a factory.
+	/// </summary>
 	class EnemyFactory
 	{
 		public static Marble createMarble(Vector2 position, PhysicsSystem physics)
 		{
 			Marble m = new Marble();
-			m.physicsProperties.fixture = FixtureFactory.CreateCircle(physics.world, 30.0f, 3.0f, position);
+			m.physicsProperties.fixture = FixtureFactory.CreateCircle(physics.world, 25.0f, 3.0f, position);
 			m.physicsProperties.fixture.Body.BodyType = BodyType.Dynamic;
 			return m;
+		}
+
+		public static Box createBox(Vector2 position, PhysicsSystem physics)
+		{
+			Box b = new Box();
+			b.physicsProperties.fixture = FixtureFactory.CreateRectangle(physics.world, 50f, 50f, 6f, position);
+			b.physicsProperties.fixture.Body.BodyType = BodyType.Static;
+			return b;
 		}
 	}
 }
