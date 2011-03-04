e4289c82a9d27a706b8679030e9bbea6af758b06
lzy5818957@gmail.com <lzy5818957@gmail.com@3d5b969a-315a-e0a3-cde1-6223939dcf86>
2011-03-04 03:48:32 +0000

    Add more features for demo

--- trunk/PhysicsDefense/GameState/GameWorld.cs
+++ trunk/PhysicsDefense/GameState/GameWorld.cs
@@ -14,6 +14,10 @@ namespace PhysicsDefense.GameState
 		PhysicsSystem physics;
 		GraphicsEngine graphics;
 
+        Boolean Lpressed, Lclicked, Rpressed, Rclicked;
+        int currScrollWheelValue;
+        Box rotationIndicator;
+
 		List<GameObject> entities;
 
 		public GameWorld(GraphicsEngine graphics)
@@ -25,22 +29,67 @@ namespace PhysicsDefense.GameState
 			// Add a temporary test enemies
 			Marble testMarble = EnemyFactory.createMarble(new Vector2(10f, 3f), physics);
 			addObject(testMarble);
+
 			Box testBox = EnemyFactory.createBox(new Vector2(10f, 24f), physics);
 			addObject(testBox);
+            testBox.physicsProperties.fixture.Body.Rotation = (float)Math.PI / 12.0f;
+
 			Box testBox2 = EnemyFactory.createBox(new Vector2(28.2f, 45f), physics);
 			addObject(testBox2);
-			testBox.physicsProperties.fixture.Body.Rotation = (float)Math.PI / 12.0f;
+			
 			Marble testMarble2 = EnemyFactory.createMarble(new Vector2(27.8f, 35f), physics);
 			addObject(testMarble2);
+
+            rotationIndicator = EnemyFactory.createBox(new Vector2(75f, 5f), physics);
+            addObject(rotationIndicator);
 		}
 
 		public void Update(GameTime gameTime)
 		{
-			// Temporary for fun
-			if(Mouse.GetState().LeftButton == ButtonState.Pressed) {
-				Marble m = EnemyFactory.createMarble(new Vector2(Mouse.GetState().X / 10f, Mouse.GetState().Y / 10f), physics);
-				addObject(m);
-			}
+            MouseState mouse = Mouse.GetState();
+
+            //ScrollWheelValue
+            currScrollWheelValue = mouse.ScrollWheelValue;
+
+			// Add Marble by mouse
+            if (mouse.RightButton == ButtonState.Pressed)
+                Lpressed = true;
+
+            if (mouse.RightButton == ButtonState.Released && Lpressed)
+            {
+                Lclicked = true;
+                Lpressed = false;
+            }
+
+            if (Lclicked)
+            {
+                Marble m = EnemyFactory.createMarble(new Vector2(Mouse.GetState().X / 10f, Mouse.GetState().Y / 10f), physics);
+                addObject(m);
+                Lclicked = false; 
+            }
+
+            // Add Box by mouse
+            if (mouse.LeftButton == ButtonState.Pressed)
+                Rpressed = true;
+
+            if (mouse.LeftButton == ButtonState.Released && Rpressed)
+            {
+                Rclicked = true;
+                Rpressed = false;
+            }
+
+            if (Rclicked)
+            {
+                Box b = EnemyFactory.createBox(new Vector2(Mouse.GetState().X / 10f, Mouse.GetState().Y / 10f), physics);                
+                addObject(b);
+                b.physicsProperties.fixture.Body.Rotation = (float)((currScrollWheelValue / 120) * Math.PI / 12.0f);
+
+                Rclicked = false;
+            }
+
+            //Direction indicator
+
+            rotationIndicator.physicsProperties.fixture.Body.Rotation = (float)((currScrollWheelValue / 120) * Math.PI / 12.0f);
 
 			// Update physics
 			physics.Update(gameTime);
