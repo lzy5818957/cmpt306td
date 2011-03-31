4e19ba64ab8df7ded5fd85c75cca1c40ea6f9b37
izenja <izenja@3d5b969a-315a-e0a3-cde1-6223939dcf86>
2011-03-31 03:38:58 +0000

    Forgot to add a file

---
+++ trunk/PhysicsDefense/KeyBindings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+
+namespace PhysicsDefense
+{
+	public class KeyBindings
+	{
+		public static Keys cancelTower = Keys.Escape;
+		public static Keys placeBasicTower = Keys.T;
+		public static Keys spin = Keys.S;
+	}
+}
